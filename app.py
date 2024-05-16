8b94910b891018b714f01974b7d12f2fe5b1c020
ripa01 <ripabiswas2739@gmail.com>
2024-05-16 19:49:57 +0600

    deploy the model with streamlit

---
+++ app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+# import the model
+pipe = pickle.load(open('pipe.pkl','rb'))
+df = pickle.load(open('df.pkl','rb'))
+
+st.title("Mobile Price Predictor")
+
+# brand
+Company = st.selectbox('Brand',df['Brand'].unique())
+
+# year
+Released_Year = st.selectbox('Released Year',df['Released Year'].unique())
+
+# OS
+Operating_System = st.selectbox('OS',df['OS'].unique())
+
+# size
+Display = st.number_input('Display (Inches)')
+
+# Camera
+Camera = st.number_input('Camera (MP)')
+
+# resolution
+Camera_Resolution= st.selectbox('Camera Resolution',df['Camera Resolution'].unique())
+
+# Ram
+Ram = st.number_input('Ram (GB)')
+
+# Battery
+Battery = st.number_input('Battery (mAh)')
+
+
+
+if st.button('Predict Price'):
+    query = np.array([Company, Released_Year, Operating_System, Display, Camera, Camera_Resolution, Ram, Battery])
+    query = query.reshape(1, -1)
+    st.title("The predicted price of this configuration mobile is " + str(int(np.exp(pipe.predict(query)[0]))) + ' TK.')
